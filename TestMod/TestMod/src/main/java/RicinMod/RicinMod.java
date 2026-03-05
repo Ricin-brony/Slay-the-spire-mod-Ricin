8839a2ef22a168fec471963a8657f7e982f76f03
Ricin-brony <314125897@qq.com>
2026-03-05 13:18:06 +0800

    基础卡组更新

--- TestMod/TestMod/src/main/java/RicinMod/RicinMod.java
+++ TestMod/TestMod/src/main/java/RicinMod/RicinMod.java
@@ -2,14 +2,46 @@ package RicinMod;
 
 import basemod.BaseMod;
 import basemod.interfaces.EditCardsSubscriber;
+import basemod.interfaces.EditCharactersSubscriber;
+import basemod.interfaces.EditKeywordsSubscriber;
+import basemod.interfaces.EditStringsSubscriber;
+import com.badlogic.gdx.graphics.Color;
 import com.evacipated.cardcrawl.modthespire.lib.SpireInitializer;
+import com.google.gson.Gson;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.localization.CharacterStrings;
+import RicinMod.cards.Defend;
 import RicinMod.cards.Strike;
+import RicinMod.characters.RicinCharacter;
+
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.nio.charset.StandardCharsets;
 
 @SpireInitializer
-public class RicinMod implements EditCardsSubscriber {
+public class RicinMod implements EditCardsSubscriber, EditStringsSubscriber, EditCharactersSubscriber, EditKeywordsSubscriber {
+    
+    private static final String CHAR_BUTTON = "RicinModResources/img/char/Character_Button.png";
+    private static final String CHAR_PORTRAIT = "RicinModResources/img/char/Character_Portrait.png";
+    private static final String BG_ATTACK_512 = "RicinModResources/img/512/bg_attack_512.png";
+    private static final String BG_SKILL_512 = "RicinModResources/img/512/bg_skill_512.png";
+    private static final String BG_POWER_512 = "RicinModResources/img/512/bg_power_512.png";
+    private static final String BG_ATTACK_1024 = "RicinModResources/img/1024/bg_attack.png";
+    private static final String BG_SKILL_1024 = "RicinModResources/img/1024/bg_skill.png";
+    private static final String BG_POWER_1024 = "RicinModResources/img/1024/bg_power.png";
+    private static final String SMALL_ORB = "RicinModResources/img/char/small_orb.png";
+    private static final String BIG_ORB = "RicinModResources/img/char/card_orb.png";
+    private static final String ENERGY_ORB = "RicinModResources/img/char/cost_orb.png";
+    
+    public static final Color RICIN_COLOR = new Color(0.0F, 26.0F / 255.0F, 255.0F / 255.0F, 1.0F);
     
     public RicinMod() {
         BaseMod.subscribe(this);
+        BaseMod.addColor(RicinCharacter.PlayerColorEnum.RICIN_BLUE, RICIN_COLOR, RICIN_COLOR, RICIN_COLOR, RICIN_COLOR,
+            RICIN_COLOR, RICIN_COLOR, RICIN_COLOR, BG_ATTACK_512, BG_SKILL_512, BG_POWER_512,
+            ENERGY_ORB, BG_ATTACK_1024, BG_SKILL_1024, BG_POWER_1024, BIG_ORB, SMALL_ORB);
     }
     
     public static void initialize() {
@@ -18,7 +50,45 @@ public class RicinMod implements EditCardsSubscriber {
     
     @Override
     public void receiveEditCards() {
-        // 向basemod注册卡牌
         BaseMod.addCard(new Strike());
+        BaseMod.addCard(new Defend());
+    }
+
+    @Override
+    public void receiveEditCharacters() {
+        BaseMod.addCharacter(new RicinCharacter(CardCrawlGame.playerName), CHAR_BUTTON, CHAR_PORTRAIT, RicinCharacter.PlayerColorEnum.RICIN_CHARACTER);
+    }
+    
+    @Override
+    public void receiveEditStrings() {
+        String lang;
+        if (Settings.language == Settings.GameLanguage.ZHS) {
+            lang = "ZHS";
+        } else {
+            lang = "ENG";
+        }
+        BaseMod.loadCustomStringsFile(CardStrings.class, "RicinModResources/localization/" + lang + "/cards.json");
+        BaseMod.loadCustomStringsFile(CharacterStrings.class, "RicinModResources/localization/" + lang + "/characters.json");
+    }
+
+    @Override
+    public void receiveEditKeywords() {
+        String lang = (Settings.language == Settings.GameLanguage.ZHS) ? "ZHS" : "ENG";
+        String path = "/RicinModResources/localization/" + lang + "/Keywords.json";
+        java.io.InputStream is = RicinMod.class.getResourceAsStream(path);
+        if (is == null) return;
+        try (Reader reader = new InputStreamReader(is, StandardCharsets.UTF_8)) {
+            Gson gson = new Gson();
+            KeywordsData[] keywords = gson.fromJson(reader, KeywordsData[].class);
+            if (keywords != null) {
+                for (KeywordsData kw : keywords) {
+                    if (kw.NAMES != null && kw.NAMES.length > 0) {
+                        BaseMod.addKeyword("ricinmod", kw.NAMES[0], kw.NAMES, kw.DESCRIPTION);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            BaseMod.logger.error("RicinMod: Failed to load keywords from " + path, e);
+        }
     }
 }
---
+++ TestMod/TestMod/src/main/java/RicinMod/characters/RicinCharacter.java
@@ -0,0 +1,192 @@
+package RicinMod.characters;
+
+import basemod.abstracts.CustomPlayer;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.evacipated.cardcrawl.modthespire.lib.SpireEnum;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.core.EnergyManager;
+import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.helpers.FontHelper;
+import com.megacrit.cardcrawl.helpers.ScreenShake;
+import com.megacrit.cardcrawl.localization.CharacterStrings;
+import com.megacrit.cardcrawl.screens.CharSelectInfo;
+import RicinMod.RicinMod;
+import RicinMod.cards.Defend;
+import RicinMod.cards.Strike;
+import RicinMod.relics.DreamChimeRelic;
+
+import java.util.ArrayList;
+
+public class RicinCharacter extends CustomPlayer {
+    private static final String SHOULDER_1 = "RicinModResources/img/char/shoulder1.png";
+    private static final String SHOULDER_2 = "RicinModResources/img/char/shoulder2.png";
+    private static final String CORPSE_IMAGE = "RicinModResources/img/char/corpse.png";
+    private static final String[] ORB_TEXTURES = new String[]{
+            "RicinModResources/img/UI/orb/layer5.png",
+            "RicinModResources/img/UI/orb/layer4.png",
+            "RicinModResources/img/UI/orb/layer3.png",
+            "RicinModResources/img/UI/orb/layer2.png",
+            "RicinModResources/img/UI/orb/layer1.png",
+            "RicinModResources/img/UI/orb/layer6.png",
+            "RicinModResources/img/UI/orb/layer5d.png",
+            "RicinModResources/img/UI/orb/layer4d.png",
+            "RicinModResources/img/UI/orb/layer3d.png",
+            "RicinModResources/img/UI/orb/layer2d.png",
+            "RicinModResources/img/UI/orb/layer1d.png"
+    };
+    private static final float[] LAYER_SPEED = new float[]{-40.0F, -32.0F, 20.0F, -20.0F, 0.0F, -10.0F, -8.0F, 5.0F, -5.0F, 0.0F};
+
+    private static final CharacterStrings characterStrings = CardCrawlGame.languagePack.getCharacterString("RicinMod:RicinCharacter");
+
+    public RicinCharacter(String name) {
+        super(name, RicinCharacter.PlayerColorEnum.RICIN_CHARACTER, ORB_TEXTURES, "RicinModResources/img/UI/orb/vfx.png", LAYER_SPEED, null, null);
+
+        this.dialogX = (this.drawX + 0.0F * Settings.scale);
+        this.dialogY = (this.drawY + 150.0F * Settings.scale);
+
+        this.initializeClass(
+                "RicinModResources/img/char/character.png",
+                SHOULDER_2, SHOULDER_1,
+                CORPSE_IMAGE,
+                this.getLoadout(),
+                0.0F, 0.0F,
+                200.0F, 220.0F,
+                new EnergyManager(3)
+        );
+    }
+
+    @Override
+    public ArrayList<String> getStartingDeck() {
+        ArrayList<String> retVal = new ArrayList<>();
+        for (int x = 0; x < 4; x++) {
+            retVal.add(Strike.ID);
+        }
+        for (int x = 0; x < 4; x++) {
+            retVal.add(Defend.ID);
+        }
+        return retVal;
+    }
+
+    @Override
+    public ArrayList<String> getStartingRelics() {
+        ArrayList<String> retVal = new ArrayList<>();
+        retVal.add(DreamChimeRelic.ID);
+        return retVal;
+    }
+
+    @Override
+    public CharSelectInfo getLoadout() {
+        return new CharSelectInfo(
+                characterStrings.NAMES[0],
+                characterStrings.TEXT[0],
+                75,
+                75,
+                0,
+                99,
+                5,
+                this,
+                this.getStartingRelics(),
+                this.getStartingDeck(),
+                false
+        );
+    }
+
+    @Override
+    public String getTitle(AbstractPlayer.PlayerClass playerClass) {
+        return characterStrings.NAMES[0];
+    }
+
+    @Override
+    public AbstractCard.CardColor getCardColor() {
+        return RicinCharacter.PlayerColorEnum.RICIN_BLUE;
+    }
+
+    @Override
+    public AbstractCard getStartCardForEvent() {
+        return new Strike();
+    }
+
+    @Override
+    public Color getCardTrailColor() {
+        return RicinMod.RICIN_COLOR;
+    }
+
+    @Override
+    public int getAscensionMaxHPLoss() {
+        return 5;
+    }
+
+    @Override
+    public BitmapFont getEnergyNumFont() {
+        return FontHelper.energyNumFontBlue;
+    }
+
+    @Override
+    public void doCharSelectScreenSelectEffect() {
+        CardCrawlGame.screenShake.shake(ScreenShake.ShakeIntensity.MED, ScreenShake.ShakeDur.SHORT, false);
+    }
+
+    @Override
+    public String getCustomModeCharacterButtonSoundKey() {
+        return "ATTACK_HEAVY";
+    }
+
+    @Override
+    public String getLocalizedCharacterName() {
+        return characterStrings.NAMES[0];
+    }
+
+    @Override
+    public AbstractPlayer newInstance() {
+        return new RicinCharacter(this.name);
+    }
+
+    @Override
+    public String getSpireHeartText() {
+        return characterStrings.TEXT[1];
+    }
+
+    @Override
+    public Color getSlashAttackColor() {
+        return RicinMod.RICIN_COLOR;
+    }
+
+    @Override
+    public String getVampireText() {
+        return com.megacrit.cardcrawl.events.city.Vampires.DESCRIPTIONS[0];
+    }
+
+    @Override
+    public Color getCardRenderColor() {
+        return RicinMod.RICIN_COLOR;
+    }
+
+    @Override
+    public AbstractGameAction.AttackEffect[] getSpireHeartSlashEffect() {
+        return new AbstractGameAction.AttackEffect[]{
+                AbstractGameAction.AttackEffect.SLASH_HEAVY,
+                AbstractGameAction.AttackEffect.FIRE,
+                AbstractGameAction.AttackEffect.SLASH_DIAGONAL,
+                AbstractGameAction.AttackEffect.SLASH_HEAVY,
+                AbstractGameAction.AttackEffect.FIRE,
+                AbstractGameAction.AttackEffect.SLASH_DIAGONAL
+        };
+    }
+
+    public static class PlayerColorEnum {
+        @SpireEnum
+        public static AbstractPlayer.PlayerClass RICIN_CHARACTER;
+
+        @SpireEnum
+        public static AbstractCard.CardColor RICIN_BLUE;
+    }
+
+    public static class PlayerLibraryEnum {
+        @SpireEnum
+        public static com.megacrit.cardcrawl.helpers.CardLibrary.LibraryType RICIN_BLUE;
+    }
+}
